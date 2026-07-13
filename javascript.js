3d7feb6f2b7f2c860a6149f18a7ba5493e173eb4
A.J.Jona <jonablessy33@gmail.com>
2026-07-13 12:16:51 +0530

    Add web project

---
+++ javascript.js
@@ -0,0 +1,665 @@
+// task__1
+
+// let a=prompt("Enter the number");
+// if (a<0){
+//     console.log("a is negative");
+// }
+// else if(a>0){
+//     console.log("a is positive");
+// }
+// else {
+//      console.log("a is zero");
+//      alert("the entered value is zero");
+// }
+
+
+// task__2
+
+// let a=prompt("Enter the number");
+// if (a%2===0){
+//     console.log("a is even");
+//     alert("even");
+// }
+// else {
+//     console.log("a is odd");
+//     alert("odd");
+// }
+
+// task__3
+
+// let a=prompt("Enter the first number");
+// let b=prompt("Enter the second number");
+// if (a>b){
+//     console.log("a is greater");
+//     alert("a is greater");
+// }
+// else if(b>a){
+//     console.log("a is greater");
+//     alert("b is greater");
+// }
+
+// task__4
+
+let a=prompt("Enter the first number");
+let b=prompt("Enter the second number");
+let c=prompt("Enter the third number");
+if ((a>b)&&(a>c)){
+    console.log("a is greater");
+    alert("a is greater");
+}
+else if((b>a)&&(b>c)){
+    console.log("a is greater");
+    alert("b is greater");
+}
+else{
+    console.log("c is greater");
+    alert("c is greater");
+}
+
+// task__5
+
+
+let ar=Number(prompt("Enter the year"));
+if (ar%4===0){
+    console.log("The given year is leap year");
+    alert("leap year");
+}
+else {
+    console.log("The given year is not an leap year");
+    alert("not a leap year");
+}
+
+// task__6
+
+// let a=prompt("Enter the character");
+// if (a==="a"||"A"||"e"||"E"||"i"||"I"||"o"||"O"||"u"||"U"){
+//     console.log("The given character is VOWEL");
+//     alert("VOWEL");
+// }
+// else{
+//      console.log("The given character is not an CONSONAT");
+//      alert("CONSONAT");
+// }
+
+
+// task__7
+
+
+// let a=prompt("Ente the Number");
+// if ((a%5===0)&&(a%11===0)){
+//     console.log("The given number can divisible by both 5 and 11");
+//     alert("can be divisible by 5 and 11");
+// }
+// else {
+//      console.log("The given number can not  divisible by both 5 and 11");
+//      alert("cannot divisible by 5 and 11");
+// }
+
+
+// task__8
+
+
+// let a=Number(prompt("Enter your mark"));
+// if (a>=90 && a<=100){
+//     console.log("Grade A");
+//     alert("A");
+// }
+// else if(a>=80&&a<90){
+//     console.log("Grade B");
+//     alert("B");
+// }
+// else if(a>=70&&a<80){
+//     console.log("Grade C");
+//     alert("C");
+// }
+// else if(a>=60&&a<70){
+//     console.log("Grade D");
+//     alert("D");``````
+// }
+// else{
+//     console.log("Grade F");
+//     alert("F");
+// }
+
+
+// tawsk__9
+
+
+// let a=Number(prompt("Enter your age"));
+// if (a<13){
+//     console.log("Child");
+//     alert("Child 👶");
+// }
+// else if(a>=13&&a<20){
+//     console.log("Teenager");
+//     alert("Teenager 💁‍♀️");
+// }
+// else if(a>=20&&a<60){
+//     console.log("Adult👩‍🦰");
+//     alert("Adult");
+// }
+// else if(a>=60){
+//     console.log("Senior Citizen");
+//     alert("Senior Citizen 👩‍🦳");``````
+// }
+
+
+// task__10
+
+
+// let dayNum = Number(prompt("Enter the number to finds its day"));
+// let day;
+
+// switch (dayNum) {
+//   case 1:
+//     day = "Sunday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   case 2:
+//     day = "Monday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   case 3:
+//     day = "Tuesday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   case 4:
+//     day = "Wednesday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   case 5:
+//     day = "Thursday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   case 6:
+//     day = "Friday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   case 7:
+//     day = "Saturday";
+//     console.log(day);
+//     alert(day);
+//     break;
+//   default:
+//     day = "Unknown Day";
+//     console.log(day);
+// }
+    
+
+
+// task__11
+
+
+// let Num = Number(prompt("Enter the number to finds its Month"));
+// let Month;
+
+// switch (Num) {
+//   case 1:
+//     Month = "Sunday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   case 2:
+//     Month = "Monday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   case 3:
+//     Month = "Tuesday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   case 4:
+//     Month = "Wednesday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   case 5:
+//     day = "Thursday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   case 6:
+//     Month = "Friday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   case 7:
+//     Month = "Saturday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//    case 8:
+//     Month = "Saturday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//    case 9:
+//     Month = "Saturday";
+//     console.log(Month);
+//     alert(Month);
+//     break;
+//   default:
+//     Month = "Unknown Day";
+//     console.log(Month);
+// }
+
+
+
+
+// for (let i=1;i<=10;i++){
+//     if (i%2===0){
+//         console.log("even",i);
+//         alert("even");
+//     }
+//     else {
+//         console.log("odd",i);
+//         alert("Odd");
+//     }
+// }
+
+// task__1
+// for (let i=1;i<=10;i++){
+//     console.log(i);
+//     alert(i);
+// }
+
+// task__2
+// for (let i=10;i>=1;i--){
+//     console.log(i);
+//     alert(i);
+// }
+
+// task__3
+
+// for (let i=1;i<=20;i++){
+//     if (i%2===0){
+//         console.log("even",i);
+//         alert("even");
+//     }
+//     else {
+//         console.log("odd",i);
+//         alert("Odd");
+//     }
+// }
+
+// task__4
+// let a;
+// for(let i=1;i<=10;i++){
+//     a=i*5
+//     console.log("i*=",a);
+//     alert(a);
+// }
+
+// task__5
+// let sum=0;
+// for (let i=1;i<=10;i++){
+//     console.log(i);
+//     alert(i);
+//     sum=sum+i
+// }
+// console.log("Sum=",sum);
+// alert(sum);
+
+
+// task__6
+// let i=1;
+// while(i<=10){
+//     console.log(i)
+//     alert(i);
+//     i++;
+// }
+
+// task__7
+// let i=20;
+// while(i>=10){
+//     console.log(i)
+//     alert(i);
+//     i--;
+// }
+
+// task__8
+
+// for (let i=1;i<=15;i++){
+//     if (i%2!=0){
+//         console.log("Odd",i);
+//         alert(i);
+//     }
+//  }
+
+// task__9
+
+// let sum=0;
+// for (let i=1;i<=20;i++){
+//     console.log(i);
+//     // alert(i);
+//     sum=sum+i
+// }
+// console.log("Sum=",sum);
+// alert(sum);
+
+
+//   task__10
+// let b=Number(prompt("Enter no.of times"))
+// let a;
+// for(let i=1;i<=b;i++){
+//     a=i*8
+//     console.log("i*=",a);
+// }
+
+
+// task__11
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+
+// task__12
+
+// let i = 5;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 15);
+
+// task__13
+
+// let i = 2;
+// do {
+//     console.log(i);
+//     i += 2;
+// } while (i <= 20);
+
+
+// task__14
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i += 2;
+// } while (i <= 25);
+
+// let i = 1;
+
+// do {
+//     if (i % 2 !== 0) { // Or: i % 2 === 1
+//         console.log(i);
+//     }
+//     i++;
+// } while (i <= 25);
+
+
+// task__15
+
+// let i=1;
+// let sum=0;
+// do{
+//    sum=sum+i
+//    i++;
+// }while(i<=15);
+// console.log("Sum",sum);
+
+// task__16
+
+// const student = {
+//   name: "Priya",
+//   age: 22,
+//   city: "Madurai"
+// };
+
+// for (let key in student) {
+//   console.log(key);
+// }
+
+
+// task__17
+
+// const employee = {
+//   id: 101,
+//   name: "John",
+//   salary: 50000
+// };
+
+// for (let key in employee) {
+//   console.log(employee[key]);
+// }
+
+// task__18
+
+// const student = {
+//   name: "Jona",
+//   age: 18,
+//   height:164
+// };
+
+// for (let key in student) {
+//   console.log(key + " : " + student[key]);
+// }
+
+
+// task__19
+
+// const car = {
+//   brand: "Toyota",
+//   color: "Black",
+//   price: 1200000
+// };
+
+// let count = 0;
+
+// for (let key in car) {
+//   count++;
+// }
+
+// console.log(count)
+
+
+// task__20
+
+// const fruits = ["Apple", "Banana", "Orange"];
+// for( let i in fruits){
+//     console.log(fruits[i]);
+// }
+
+
+
+
+// task__21
+
+// const colors = ["Red", "Green", "Blue"];
+// for (let color of colors) {
+//     console.log(color);
+// }
+
+// task__22
+
+// let sum=0
+// const num = [10,20,30];
+// for (let no of num) {
+//     sum=sum+no
+// }
+// console.log(sum);
+
+
+// task__23
+
+// const str= "javascript";
+// for (let char of str) {
+//     console.log(char);
+// }
+
+
+// task__24
+
+// let count=0
+// const str = "programming";
+// for (let char of str) {
+//     count++
+// }
+// console.log(count)
+
+// task__25
+
+// const num = [1,2,3,4,5,6,7,8,9,10];
+// for (let no of num) {
+//     if(no%2===0){
+//         console.log(no);
+//     }
+// }
+
+
+
+// 08/07
+// let a=Number(prompt("Enter the first number"));
+// let b=Number(prompt("Enter the second number"));
+// let sum=a+b;
+// let sub=a-b;
+// let mul=a*b;
+// let div=a/b;
+// console.log("Sum of two numbers",sum);
+// console.log("Subtraction of two numbers",sub);
+// console.log("multiplication of two numbers",mul);
+// console.log("Division of two numbers",div);
+
+
+
+
+// let mark=prompt("Enter your mark");
+// let attendance=prompt("Enter your attendance");
+// if (mark>=35 && attendance>75){
+//     console.log("Passed");
+// }
+// else{
+//     console.log("Failed")
+// }
+
+// let value=Number(prompt("Enter the number"));
+// while (value<10){
+//       value++
+//       console.log(value)
+// }
+
+// while(value<1510){
+//      ++value
+//       console.log(value);
+// }
+ 
+// task__1
+// let fruits = ["Apple", "Banana"];
+// fruits.push("Orange");
+// console.log(fruits);
+
+// task__2
+// let fruits = ["Apple", "Banana","Orange"];
+// fruits.pop();
+// console.log(fruits);
+
+// task__3
+// let fruits = ["Apple", "Banana"];
+// fruits.unshift("Orange");
+// console.log(fruits);
+
+// task__3
+// let fruits = ["Orange","Apple", "Banana"];
+// fruits.shift();
+// console.log(fruits);
+
+// task__4
+// let fruits = ["Orange","Apple", "Banana"];
+// console.log(fruits.length);
+
+// task__5
+// let fruits = ["javascript", "java", "python"];
+// console.log(fruits.includes("java"));
+
+// task__6
+// let fruits = ["javascript", "java", "python"];
+// console.log(fruits.indexOf("java"));
+
+// task__7
+// let numbers = [10, 20, 30, 20, 40];
+// console.log(numbers.lastIndexOf(20));
+
+// task__8
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.reverse();
+// console.log(numbers);
+
+// task__9
+// let numbers = [40, 10, 30, 20];
+// numbers.sort((a, b) => a - b);
+// console.log(numbers);
+
+// task_10
+// let fruits = ["Orange", "Apple", "Banana"];
+// fruits.sort();
+// console.log(fruits);
+
+// task__11
+// let fruits = ["Apple", "Banana", "Orange"];
+// console.log(fruits.join(","));
+
+// task__12
+// let numbers = [10, 20, 30, 40, 50];
+// let result = numbers.slice(0, 3);
+// console.log(result);
+
+// task__13
+// let numbers = [10, 20, 30, 40, 50];
+// numbers.splice(2, 2);
+// console.log(numbers);
+
+// task__14
+// let numbers = [10, 20, 30, 40, 50];
+// numbers.reverse();
+// console.log(numbers);
+
+
+// task__15
+// let courses = ["HTML", "CSS", "React"];
+// let updatedCourses = courses.map(course =>
+//   course === "HTML" ? "JavaScript" : course);
+// console.log(updatedCourses);
+
+// task__16
+// let a=[0,2,4,6];
+// let b=[1,3,5,7,9];
+// let c=a.concat(b);
+// console.log(c);
+
+// task__17
+// let numbers = [1, 2, 3];
+// let result = numbers.map(num => num * 2);
+// console.log(result);
+
+
+
+//task__18
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let evenNumbers = numbers.filter(num => num % 2 === 0);
+// console.log(evenNumbers);
+
+// let number = [10, 15, 20, 25, 30, 35];
+// let result = number.filter(num => num % 2 === 0);
+// console.log(result);
+
+
+// task__19
+// let numbers = [10,20,30,40,50,60,70,80,90,100];
+// let result = numbers.find(num => num > 50);
+// console.log(result);
+
+
+// task__20
+// const array = [5,10,24,17];
+// const sum = array.reduce((acc, curr) => acc + curr);
+// console.log(sum);
